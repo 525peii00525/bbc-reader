b2574ea7e5727571262676b5104195e1bf5f9e1a
525peii00525 <shinpei062@gmail.com>
2026-07-03 11:16:52 +0900

    Add health check endpoint

---
+++ backend/bbc-reader-api/src/main/java/com/example/bbc_reader_api/BbcReaderApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.bbc_reader_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbcReaderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbcReaderApiApplication.class, args);
+	}
+
+}
